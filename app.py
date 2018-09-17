460d36b82f0e22675271afee0eb9efc81d02deeb
mohsin e nur <Nur Mohsin>
2018-09-17 20:55:05 +0600

    uploading worked project first time

---
+++ app.py
@@ -0,0 +1,385 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField
+from passlib.hash import sha256_crypt
+from functools import wraps
+from flask_mail import Mail, Message
+import os
+
+from wtforms.fields.html5 import EmailField
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+# Config MySQL
+mysql = MySQL()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'shoptubedb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Initialize the app for use with this MySQL class
+mysql.init_app(app)
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, *kwargs)
+        else:
+            flash('Unauthorized, Please logged in', 'danger')
+            return redirect(url_for('login'))
+
+    return wrap
+
+
+def not_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            flash('Unauthorized, You logged in', 'danger')
+            return redirect(url_for('index'))
+        else:
+            return f(*args, *kwargs)
+
+    return wrap
+
+
+def is_admin_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'admin_logged_in' in session:
+            return f(*args, *kwargs)
+        else:
+            flash('Unauthorized, Please logged in', 'danger')
+            return redirect(url_for('admin_login'))
+
+    return wrap
+
+
+def not_admin_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'admin_logged_in' in session:
+            return f(*args, *kwargs)
+        else:
+            flash('Unauthorized, You logged in', 'danger')
+            return redirect(url_for('admin_login'))
+
+    return wrap
+
+
+@app.route('/')
+def index():
+    form = OrderForm(request.form)
+    # Create cursor
+    cur = mysql.connection.cursor()
+    # Get message
+    values = 'women'
+    cur.execute("SELECT * FROM products WHERE category=%s ORDER BY id ASC LIMIT 8", (values,))
+    products = cur.fetchall()
+    # Close Connection
+    cur.close()
+    return render_template('home.html', products=products, form=form)
+
+
+class LoginForm(Form):  # Create Login Form
+    username = StringField('', [validators.length(min=1)],
+                           render_kw={'autofocus': True, 'placeholder': 'Username'})
+    password = PasswordField('', [validators.length(min=3)],
+                             render_kw={'placeholder': 'Password'})
+
+
+# User Login
+@app.route('/login', methods=['GET', 'POST'])
+@not_logged_in
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # GEt user form
+        username = form.username.data
+        # password_candidate = request.form['password']
+        password_candidate = form.password.data
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username=%s", [username])
+
+        if result > 0:
+            # Get stored value
+            data = cur.fetchone()
+            password = data['password']
+            uid = data['id']
+            name = data['name']
+
+            # Compare password
+            if sha256_crypt.verify(password_candidate, password):
+                # passed
+                session['logged_in'] = True
+                session['uid'] = uid
+                session['s_name'] = name
+                x = '1'
+                cur.execute("UPDATE users SET online=%s WHERE id=%s", (x, uid))
+                flash('You are now logged in', 'success')
+
+                return redirect(url_for('index'))
+
+            else:
+                flash('Incorrect password', 'danger')
+                return render_template('login.html', form=form)
+
+        else:
+            flash('Username not found', 'danger')
+            # Close connection
+            cur.close()
+            return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
+
+
+@app.route('/out')
+def logout():
+    if 'uid' in session:
+        # Create cursor
+        cur = mysql.connection.cursor()
+        uid = session['uid']
+        x = '0'
+        cur.execute("UPDATE users SET online=%s WHERE id=%s", (x, uid))
+        session.clear()
+        flash('You are logged out', 'success')
+        return redirect(url_for('index'))
+    return redirect(url_for('login'))
+
+
+class RegisterForm(Form):
+    name = StringField('', [validators.length(min=3, max=50)],
+                       render_kw={'autofocus': True, 'placeholder': 'Full Name'})
+    username = StringField('', [validators.length(min=3, max=25)], render_kw={'placeholder': 'Username'})
+    email = EmailField('', [validators.DataRequired(), validators.Email(), validators.length(min=4, max=25)],
+                       render_kw={'placeholder': 'Email'})
+    password = PasswordField('', [validators.length(min=3)],
+                             render_kw={'placeholder': 'Password'})
+    mobile = StringField('', [validators.length(min=11, max=15)], render_kw={'placeholder': 'Mobile'})
+
+
+@app.route('/register', methods=['GET', 'POST'])
+@not_logged_in
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        mobile = form.mobile.data
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password, mobile) VALUES(%s, %s, %s, %s, %s)",
+                    (name, email, username, password, mobile))
+
+        # Commit cursor
+        mysql.connection.commit()
+
+        # Close Connection
+        cur.close()
+
+        flash('You are now registered and can login', 'success')
+
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+class MessageForm(Form):  # Create Message Form
+    body = StringField('', [validators.length(min=1)], render_kw={'autofocus': True})
+
+
+@app.route('/chatting/<string:id>', methods=['GET', 'POST'])
+def chatting(id):
+    if 'uid' in session:
+        form = MessageForm(request.form)
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # lid name
+        get_result = cur.execute("SELECT * FROM users WHERE id=%s", [id])
+        l_data = cur.fetchone()
+        if get_result > 0:
+            session['name'] = l_data['name']
+            uid = session['uid']
+            session['lid'] = id
+
+            if request.method == 'POST' and form.validate():
+                txt_body = form.body.data
+                # Create cursor
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO messages(body, msg_by, msg_to) VALUES(%s, %s, %s)",
+                            (txt_body, id, uid))
+                # Commit cursor
+                mysql.connection.commit()
+
+            # Get users
+            cur.execute("SELECT * FROM users")
+            users = cur.fetchall()
+
+            # Close Connection
+            cur.close()
+            return render_template('chat_room.html', users=users, form=form)
+        else:
+            flash('No permission!', 'danger')
+            return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/chats', methods=['GET', 'POST'])
+def chats():
+    if 'lid' in session:
+        id = session['lid']
+        uid = session['uid']
+        # Create cursor
+        cur = mysql.connection.cursor()
+        # Get message
+        cur.execute("SELECT * FROM messages WHERE (msg_by=%s AND msg_to=%s) OR (msg_by=%s AND msg_to=%s) "
+                    "ORDER BY id ASC", (id, uid, uid, id))
+        chats = cur.fetchall()
+        # Close Connection
+        cur.close()
+        return render_template('chats.html', chats=chats, )
+    return redirect(url_for('login'))
+
+
+@app.route('/mens')
+def mens():
+    # Create cursor
+    cur = mysql.connection.cursor()
+    # Get message
+    values = 'men'
+    cur.execute("SELECT * FROM products WHERE category=%s ORDER BY id ASC", (values,))
+    products = cur.fetchall()
+    # Close Connection
+    cur.close()
+    return render_template('mens.html', products=products)
+
+
+class OrderForm(Form):  # Create Order Form
+    name = StringField('', [validators.length(min=1), validators.DataRequired()],
+                       render_kw={'autofocus': True, 'placeholder': 'Full Name'})
+    mobile_num = StringField('', [validators.length(min=1), validators.DataRequired()],
+                             render_kw={'autofocus': True, 'placeholder': 'Mobile'})
+    quantity = SelectField('', [validators.DataRequired()], choices=[('1', '1'), ('2', '2')])
+    order_place = StringField('', [validators.length(min=1), validators.DataRequired()],
+                              render_kw={'placeholder': 'Order Place'})
+    pid = StringField('', [validators.length(min=1), validators.DataRequired()], render_kw={'readonly': True})
+
+
+@app.route('/womens', methods=['GET', 'POST'])
+def womens():
+    form = OrderForm(request.form)
+    # Create cursor
+    cur = mysql.connection.cursor()
+    # Get message
+    values = 'women'
+    cur.execute("SELECT * FROM products WHERE category=%s ORDER BY id ASC", (values,))
+    products = cur.fetchall()
+    # Close Connection
+    cur.close()
+
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        mobile = form.mobile_num.data
+        order_place = form.order_place.data
+        quantity = form.quantity.data
+        pid = form.pid.data
+
+        # Create Cursor
+        curs = mysql.connection.cursor()
+        curs.execute("INSERT INTO orders(pid, ofname, mobile, oplace, quantity) VALUES(%s, %s, %s, %s, %s)",
+                     (pid, name, mobile, order_place, quantity))
+
+        # Commit cursor
+        mysql.connection.commit()
+
+        # Close Connection
+        cur.close()
+
+        flash('Order successful', 'success')
+        return render_template('womens.html', products=products, form=form)
+    return render_template('womens.html', products=products, form=form)
+
+
+@app.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # GEt user form
+        username = form.username.data
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM admin WHERE username=%s", [username])
+
+        if result > 0:
+            # Get stored value
+            data = cur.fetchone()
+            password = data['password']
+            uid = data['id']
+            name = data['name']
+
+            # Compare password
+            if sha256_crypt.verify(password_candidate, password):
+                # passed
+                session['logged_in'] = True
+                session['uid'] = uid
+                session['s_name'] = name
+                x = '1'
+                cur.execute("UPDATE users SET online=%s WHERE id=%s", (x, uid))
+                flash('You are now logged in', 'success')
+
+                return redirect(url_for('index'))
+
+            else:
+                flash('Incorrect password', 'danger')
+                return render_template('admin_login.html', form=form)
+
+        else:
+            flash('Username not found', 'danger')
+            # Close connection
+            cur.close()
+            return render_template('admin_login.html', form=form)
+    return render_template('admin_login.html', form=form)
+
+
+@app.route('/admin')
+def admin():
+    return render_template('pages/index.html')
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    form = OrderForm(request.form)
+    if 'q' in request.args:
+        q = request.args['q']
+        # Create cursor
+        cur = mysql.connection.cursor()
+        # Get message
+        query_string = "SELECT * FROM products WHERE pName LIKE %s ORDER BY id ASC"
+        cur.execute(query_string, (q,))
+        products = cur.fetchall()
+        # Close Connection
+        cur.close()
+        flash('Showing result for: ' + q, 'success')
+        return render_template('search.html', products=products, form=form)
+    else:
+        flash('Search again', 'danger')
+        return render_template('search.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
